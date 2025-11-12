959a1ad09d60e2c676a1971043358a82207fe559
gourav-hiwale136 <gauravhiwale136@gmail.com>
2025-11-12 18:11:34 +0530

    12/11/25

--- frontend/react-app/src/pages/Signup.jsx
+++ frontend/react-app/src/pages/Signup.jsx
@@ -1,53 +1,47 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import axios from 'axios'
 import "../styles/Signup.css";
 
-export default function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
-    console.log("Username", username)
-    // You can add your signup logic here (API call or Firebase)
-    navigate("/login"); // redirect to login page after signup
-  };
+const Signup = () => {
 
+  const [Userdata,setUserdata] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
+
+    const handlechange = (e) => {
+      setUserdata({ ...Userdata, [e.target.name]: e.target.value });
+    };
+
+
+    const signFun = async(event) => {
+      event.preventDefault()
+      try {
+        const response = await axios.post("http://localhost:8000/user/signup", Userdata);
+        console.log("Signup Successfull", response.data)
+        alert("Signup Successfull !")
+      } catch (error) {
+        console.log("Signup Failed", error)
+        alert("Error Signup !")
+      }
+      };
+      
+   
   return (
     <div className="signup-container">
-      <form onSubmit={handleSubmit} className="signup-form">
-        <h2>Create Account</h2>
-
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-
-        <input
-          type="username"
-          placeholder="Enter your Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-
-        <button type="submit">Sign Up</button>
+      <form onSubmit={signFun} className="signup-form">
+        <h2>Signup</h2>
+        <input type="text"  onChange={handlechange} name='name'  placeholder='User name ...' value={Userdata.name}/>
+        <input type="email"  onChange={handlechange} name='email' placeholder='E-mail...' value={Userdata.email}/>
+        <input type="password" onChange={handlechange} name='password' placeholder='Password...' value={Userdata.password}/>
+       <button type="submit">Signup</button>
       </form>
     </div>
+
+    
   );
 }
+
+export default Signup
