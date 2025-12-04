182da0e60e74df68f06a17f86e1d050ecf3cd91f
gourav-hiwale136 <gauravhiwale136@gmail.com>
2025-12-04 16:21:46 +0530

    04/12/25

--- Frontend_/src/pages/Appointments.jsx
+++ Frontend_/src/pages/Appointments.jsx
@@ -150,8 +150,7 @@ const Appointments = () => {
             />
             <button
               className="px-4 py-2 bg-blue-600 text-white rounded"
-              onClick={handleCreate}
-            >
+              onClick={handleCreate}>
               Add
             </button>
           </div>
